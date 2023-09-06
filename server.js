898f2d793b947a8eb697df865b3fb065073311b4
Emerson Barros <emerson.sbarros3@gmail.com>
2023-09-06 13:57:25 -0300

    update backend

--- server.js
+++ server.js
@@ -1,37 +1,12 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/db.json');
-// });
-
-// app.listen(port, () => {
-//   console.log("O servidor está rodando na porta " + port)
-// });
-
-// JSON Server module
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-
-// Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
-server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/db.json');
 });
 
-// Export the Server API
-module.exports = server;
+app.listen(port, () => {
+  console.log("O servidor está rodando na porta " + port)
+});
